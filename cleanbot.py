06d9712803c407f303e7a5b4bf67314c2503bce3
Jezzythekid <jessedegraven@gmail.com>
2025-06-16 23:04:09 +0200

    created simple program to send a message on run

---
+++ cleanbot.py
@@ -0,0 +1,35 @@
+# Sample code from the documentation
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_CHANNEL = int(os.getenv('DISCORD_CHANNEL'))
+
+import discord
+
+import time
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    channel = client.get_channel(DISCORD_CHANNEL)
+    await channel.send('dit is een test')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run(DISCORD_TOKEN)
